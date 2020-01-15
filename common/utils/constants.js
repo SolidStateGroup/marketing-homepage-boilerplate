59a8a5233eb9eb08409787075789d6ef7fa83b65
kyle <kyle@solidstategroup.com>
2020-01-15 15:59:21 +0000

    Initial

--- common/utils/constants.js
+++ common/utils/constants.js
@@ -9,23 +9,18 @@ const Constants = global.Constants = {
     },
     // <title>
     titles: {
-        home: 'Solid State Group', // Used by default on all pages
-        blog: 'Blog | Solid State Group',
-        work: 'Work | Solid State Group',
-        partners: 'Partners | Solid State Group',
-        jobs: 'Jobs | Solid State Group',
-        services: 'Services | Solid State Group',
-        contact: 'Contact | Solid State Group',
+        home: 'Site name', // Used by default on all pages
+        blog: 'Blog | Site name',
     },
     // meta:description
     descriptions: {
         // Used by default
-        home: 'At Solid State Group, we help design and build new digital products, drag legacy technology up to date, and make all your systems work together.',
+        home: 'This is your website description',
     },
     // meta:description
     keywords: {
         // Used by default
-        home: 'Api development, custom, iot, bespoke, software development, mobile application development, user interface design, ux design, react native, systems integration, cloud infrastructure, technology, digital',
+        home: 'marketing, business',
     },
     pages: {
 
